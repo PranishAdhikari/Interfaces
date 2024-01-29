57152f44f6365bbf278f35248af10d93b116ae06
pranish <adhikapi@mail.uc.edu>
2024-01-29 12:16:28 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,53 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.awt.*;
+import java.util.ArrayList;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ShortWordFilter filter = new ShortWordFilter();
+        BigRectFilter lister = new BigRectFilter();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        ArrayList <String> line = new ArrayList<>();
+        line.add(new String("Hello "));
+        line.add(new String("my"));
+        line.add(new String("name"));
+        line.add(new String("is"));
+        line.add(new String("Pranish"));
+        line.add(new String("and"));
+        line.add(new String("I"));
+        line.add(new String("like"));
+        line.add(new String("Apples"));
+
+        ArrayList <Rectangle> rect = new ArrayList<>();
+        rect.add(new Rectangle(5, 4));
+        rect.add(new Rectangle(5, 6));
+        rect.add(new Rectangle(1, 2));
+        rect.add(new Rectangle(1, 1));
+        rect.add(new Rectangle(3, 2));
+        rect.add(new Rectangle(6, 3));
+
+        String[] fields;
+        for (String l:line)
+        {
+            fields = l.split(" ");
+            for (String w: fields)
+            {
+                if (filter.accept(w))
+                {
+                    System.out.println(w.trim());
+                }
+            }
         }
+
+        System.out.println();
+        for (Rectangle r: rect)
+        {
+            if (lister.accept(r))
+            {
+                System.out.println(r);
+            }
+        }
+
     }
 }
